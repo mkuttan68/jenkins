97c4d97f2dd8c47ebaf07c2cf4a4953b674ce0fa
Manukuttan <manukuttan.52@wipro.com>
2019-01-21 10:25:59 +0530

    MavenDemo

---
+++ src/main/java/com/srcclr/Main.java
@@ -0,0 +1,27 @@
+package com.srcclr;
+
+import org.apache.commons.fileupload.MultipartStream;
+import org.apache.xml.security.signature.XMLSignatureInput;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.io.ByteArrayInputStream;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String candidate = args[0];
+    String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
+
+    BCrypt.checkpw(candidate, hashed);
+
+    filterXMLSignature();
+  }
+
+  private static void filterXMLSignature() {
+    byte[] bytes = new byte[256];
+
+    new MultipartStream(new ByteArrayInputStream(bytes), bytes);
+
+    new XMLSignatureInput(bytes).addNodeFilter(null);
+  }
+}
